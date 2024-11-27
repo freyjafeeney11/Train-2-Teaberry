5cd462eb31202ee8ebffd736d481ca3e5ab7388d
Tsering Choekyi Lhakhang <tlhakha1@uvm.edu>
2024-11-26 22:49:08 -0500

    Able to select/highlight item

--- Inventory-System/Assets/Scripts/UI/InventoryItem.cs
+++ Inventory-System/Assets/Scripts/UI/InventoryItem.cs
@@ -1,14 +1,78 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
 
 public class InventoryItem : MonoBehaviour
 {
-    void Start()
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField]
+    private TMP_Text quantityText;
+    [SerializeField]
+    private Image borderImage;
+    public event Action<InventoryItem> OnItemClicked, OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag, OnRightMouseBtnClick;
+    private bool empty = true;
+
+    public void Awake()
     {
+        ResetData();
+        Deselect();
+    }
+
+    // Hide image and set bool to true if reset
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
 
     }
-    void Update()
+    public void Deselect()
     {
-
+        borderImage.enabled = false;
+    }
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityText.text = quantity + "";
+        empty = false;
+    }
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+    public void OnBeginDrag()
+    {
+        if (empty)
+        {
+            return;
+        }
+        OnItemBeginDrag?.Invoke(this);
+    }
+    public void OnDrop()
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+    public void OnPointerClick(BaseEventData data)
+    {
+        if (empty)
+        {
+            return;
+        }
+        PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
     }
 }
--- Inventory-System/Assets/Scripts/UI/InventoryPage.cs
+++ Inventory-System/Assets/Scripts/UI/InventoryPage.cs
@@ -9,9 +9,20 @@ public class InventoryPage : MonoBehaviour
 
     [SerializeField]
     private RectTransform contentPanel;
+    [SerializeField]
+    private InventoryDescription itemDescription;
 
     List<InventoryItem> listofUIItems = new List<InventoryItem>();
 
+    public Sprite image;
+    public int quantity;
+    public string title, description;
+
+    private void Awake()
+    {
+        Hide();
+        itemDescription.ResetDescription();
+    }
     public void InitializeInventoryUI(int inventorysize)
     {
         for (int i = 0; i < inventorysize; i++)
@@ -19,16 +30,47 @@ public class InventoryPage : MonoBehaviour
             InventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
             uiItem.transform.SetParent(contentPanel);
             listofUIItems.Add(uiItem);
+
+            // Actions
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
     }
-
+    
     public void Show()
     {
         gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+        listofUIItems[0].SetData(image, quantity);
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    public void HandleItemSelection(InventoryItem obj)
+    {
+        itemDescription.SetDescription(image, title, description);
+        listofUIItems[0].Select();
+    }
+    public void HandleBeginDrag(InventoryItem obj)
+    {
+        
+    }
+    public void HandleSwap(InventoryItem obj)
+    {
+        
+    }
+    public void HandleEndDrag(InventoryItem obj)
+    {
+        
+    }
+    public void HandleShowItemActions(InventoryItem obj)
+    {
+        
     }
 }
